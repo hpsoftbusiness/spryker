024539d280533e63b35a3f7dfc4be8c248a4b2fd
Roman Shopin <shopin.roman@gmail.com>
2020-11-23 11:56:26 +0200

    Added Adyen Propel models

---
+++ src/Orm/Zed/Adyen/Persistence/SpyPaymentAdyenQuery.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Orm\Zed\Adyen\Persistence;
+
+use Orm\Zed\Adyen\Persistence\Base\SpyPaymentAdyenQuery as BaseSpyPaymentAdyenQuery;
+
+/**
+ * Skeleton subclass for performing query and update operations on the 'spy_payment_adyen' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ */
+class SpyPaymentAdyenQuery extends BaseSpyPaymentAdyenQuery
+{
+
+}
