f08c0098f50e460cad62f203e2dff2e44af215f1
Alex Galych <oleksandr.galych@spryker.com>
2020-12-08 20:54:19 +0100

    dev * Fixed affiliate link.

--- src/Pyz/Service/ProductAffiliate/ProductAffiliateConfig.php
+++ src/Pyz/Service/ProductAffiliate/ProductAffiliateConfig.php
@@ -11,7 +11,7 @@ use Spryker\Service\Kernel\AbstractBundleConfig;
 
 class ProductAffiliateConfig extends AbstractBundleConfig
 {
-    protected const TRACKING_URL_PATH = 'http://click.myworld.com/sprykerAwin';
+    protected const TRACKING_URL_PATH = 'https://click.myworld.com/spryker';
     protected const TRACKING_URL_NETWORK_ARGUMENT = 'AW_Dach';
 
     /**
