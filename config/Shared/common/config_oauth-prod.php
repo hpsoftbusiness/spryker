ff01c2781966da133b4e8357d93e6e90be8c5445
Aleksey Kotsuba <aleksey.kotsuba@gmail.com>
2020-12-11 13:46:36 +0200

    Added configuration for PROD.

--- config/Shared/common/config_oauth-prod.php
+++ config/Shared/common/config_oauth-prod.php
@@ -11,8 +11,8 @@ use Spryker\Shared\Oauth\OauthConstants;
 
 $config[OauthConstants::PRIVATE_KEY_PATH] = 'file://' . APPLICATION_ROOT_DIR . '/config/Zed/dev_only_private.key';
 $config[OauthConstants::PUBLIC_KEY_PATH] = 'file://' . APPLICATION_ROOT_DIR . '/config/Zed/dev_only_public.key';
-$config[OauthConstants::ENCRYPTION_KEY] = 'lxZFUEsBCJ2Yb14IF2ygAHI5N4+ZAUXXaSeeJm6+twsUmIen';
-$config[OauthConstants::OAUTH_CLIENT_IDENTIFIER] = 'frontend';
-$config[OauthConstants::OAUTH_CLIENT_SECRET] = 'abc123';
+$config[OauthConstants::ENCRYPTION_KEY] = getenv('OAUTH_ENCRYPTION_KEY') ?: 'lxZFUEsBCJ2Yb14IF2ygAHI5N4+ZAUXXaSeeJm6+twsUmIen';
+$config[OauthConstants::OAUTH_CLIENT_IDENTIFIER] = getenv('OAUTH_CLIENT_IDENTIFIER') ?: 'frontend';
+$config[OauthConstants::OAUTH_CLIENT_SECRET] = getenv('OAUTH_CLIENT_SECRET') ?: 'abc123';
 
-$config[AuthConstants::AUTH_DEFAULT_CREDENTIALS]['yves_system']['token'] = 'JDJ5JDEwJFE0cXBwYnVVTTV6YVZXSnVmM2l1UWVhRE94WkQ4UjBUeHBEWTNHZlFRTEd4U2F6QVBqejQ2';
+$config[AuthConstants::AUTH_DEFAULT_CREDENTIALS]['yves_system']['token'] = getenv('OAUTH_TOKEN') ?: 'JDJ5JDEwJFE0cXBwYnVVTTV6YVZXSnVmM2l1UWVhRE94WkQ4UjBUeHBEWTNHZlFRTEd4U2F6QVBqejQ2';
