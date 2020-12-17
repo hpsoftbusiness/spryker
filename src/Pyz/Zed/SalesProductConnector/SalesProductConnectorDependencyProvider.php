6f7b138b05c8e75fde07542b1df8cbaa979c6630
Oleksandr Myrnyi <oleksandr.myrnyi@spryker.com>
2020-12-17 08:59:17 +0200

    MYW-213: Localized attributes v2

---
+++ src/Pyz/Zed/SalesProductConnector/SalesProductConnectorDependencyProvider.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Pyz\Zed\SalesProductConnector;
+
+use Spryker\Zed\Kernel\Container;
+use Spryker\Zed\SalesProductConnector\SalesProductConnectorDependencyProvider as SprykerSalesProductConnectorDependencyProvider;
+
+class SalesProductConnectorDependencyProvider extends SprykerSalesProductConnectorDependencyProvider
+{
+    public const FACADE_LOCALE = 'FACADE_LOCALE';
+
+    /**
+     * @param \Spryker\Zed\Kernel\Container $container
+     *
+     * @return \Spryker\Zed\Kernel\Container
+     */
+    public function provideBusinessLayerDependencies(Container $container)
+    {
+        $container = parent::provideBusinessLayerDependencies($container);
+
+        $container = $this->addLocaleFacade($container);
+
+        return $container;
+    }
+
+    /**
+     * @param \Spryker\Zed\Kernel\Container $container
+     *
+     * @return \Spryker\Zed\Kernel\Container
+     */
+    protected function addLocaleFacade(Container $container): Container
+    {
+        $container->set(static::FACADE_LOCALE, function (Container $container) {
+            return $container->getLocator()->locale()->facade();
+        });
+
+        return $container;
+    }
+}
