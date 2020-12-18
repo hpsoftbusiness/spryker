08688ac758b4bd4b9764374cae162dbde9583db8
Alex Galych <oleksandr.galych@spryker.com>
2020-12-18 19:15:12 +0100

    feature/adyen-austria-account * updated credentials.

--- config/Shared/common/config_adyen-development.php
+++ config/Shared/common/config_adyen-development.php
@@ -16,14 +16,15 @@ $adyenCredentials = [
     'CHECKOUT_SHOPPER_API_VERSION' => '3.9.4',
     'JS_INTEGRITY_HASH' => utf8_encode('sha384-8Q8tz/+hf+UkS01nLrKLJgQLdaR1hRklqJQksCHh903UIfW+xMt275Lms4GZgVUi'),
     'CSS_INTEGRITY_HASH' => utf8_encode('sha384-6qrXvoxlnBlrflZQ9g5Yf5oZapUSSXctPxacP9oRcEukbEO7lXisuSyMKG8pDX8V'),
-    'ORIGIN_KEY' => utf8_encode('pub.v2.8216068210552874.aHR0cHM6Ly95dmVzLmRlLm15d29ybGQubG9jYWw.HI8SeXyRatdpQ27g5MDuFn0alk2x2mU4RQThBisHO8g'),
-    'API_KEY' => utf8_encode('AQEphmfxL4rMbBNHw0m/n3Q5qf3VZZJ6AoFGXJ3Q+WcpWR/tGa4apIs+LM4QwV1bDb7kfNy1WIxIIkxgBw==-KC/QMH9oO7negekCXG4P4oa04Pkl+lAnlj8aTyf3DU4=-@3*Ek{pf^6fK>Z$m'),
+    'ORIGIN_KEY' => utf8_encode('pub.v2.8216083088630330.aHR0cHM6Ly95dmVzLmRlLm15d29ybGQubG9jYWw.Oj2BH6hYCxdKyhN-clRm33LZxptO0fHBRcawFOJ0fOw'),
+    'API_KEY' => utf8_encode('AQEphmfxJo7PYxFGw0m/n3Q5qf3VZZJ6AoFGXCNIwqQyci176r2VtV9VlQ4QwV1bDb7kfNy1WIxIIkxgBw==-96Rhip3CgIMz8hUzAae+lAgvsmVFMWe+jToJzZ4j2ec=-r9CWr}:Vg&q.=pL2'),
     'CHECKOUT_API_DOMAIN' => 'checkout-test.adyen.com',
     'CHECKOUT_API_VERSION' => 'v32',
     'PAYMENT_API_DOMAIN' => 'pal-test.adyen.com',
     'PAYMENT_API_VERSION' => 'v30',
-    'MERCHANT_ACCOUNT' => 'MyWorldInternationalLimited',
+    'MERCHANT_ACCOUNT' => 'MyWorldAustriaGmbH',
     'SDK_ENVIRONMENT' => 'test',
+    //ws_941844@Company.MyWorld
 ];
 
 $config[AdyenConstants::MERCHANT_ACCOUNT] = $adyenCredentials['MERCHANT_ACCOUNT'];
