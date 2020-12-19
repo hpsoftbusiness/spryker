c887144c5492cc03749f0389d725f90c869a9bdd
Alex Galych <oleksandr.galych@spryker.com>
2020-12-19 18:08:34 +0100

    dev * Remove adyen test prod credentials.

--- config/Shared/common/config_adyen-prod.php
+++ config/Shared/common/config_adyen-prod.php
@@ -10,24 +10,7 @@ use SprykerEco\Shared\AdyenApi\AdyenApiConstants;
 
 // >>> ADYEN
 
-//TODO Set correct variables to env variables.
-//$adyenCredentials = json_decode(getenv('SPRYKER_ADYEN_CREDENTIALS') ?: 'null', true);
-
-$adyenCredentials = [
-    'CHECKOUT_SHOPPER_API_DOMAIN' => 'checkoutshopper-test.adyen.com',
-    'CHECKOUT_SHOPPER_API_VERSION' => '3.9.4',
-    'JS_INTEGRITY_HASH' => utf8_encode('sha384-8Q8tz/+hf+UkS01nLrKLJgQLdaR1hRklqJQksCHh903UIfW+xMt275Lms4GZgVUi'),
-    'CSS_INTEGRITY_HASH' => utf8_encode('sha384-6qrXvoxlnBlrflZQ9g5Yf5oZapUSSXctPxacP9oRcEukbEO7lXisuSyMKG8pDX8V'),
-    'ORIGIN_KEY' => utf8_encode('pub.v2.8216083088630330.aHR0cHM6Ly93d3cubWFya2V0cGxhY2UubXl3b3JsZC5jb20.tWAZKfLBvm9nTVd_tQNRGU8ra7hIRqFX-zbi4YWJFp4'),
-    'API_KEY' => utf8_encode('AQEphmfxJo7PYxFGw0m/n3Q5qf3VZZJ6AoFGXCNIwqQyci176r2VtV9VlQ4QwV1bDb7kfNy1WIxIIkxgBw==-96Rhip3CgIMz8hUzAae+lAgvsmVFMWe+jToJzZ4j2ec=-r9CWr}:Vg&q.=pL2'),
-    'CHECKOUT_API_DOMAIN' => 'checkout-test.adyen.com',
-    'CHECKOUT_API_VERSION' => 'v32',
-    'PAYMENT_API_DOMAIN' => 'pal-test.adyen.com',
-    'PAYMENT_API_VERSION' => 'v30',
-    'MERCHANT_ACCOUNT' => 'MyWorldAustriaGmbH',
-    'SDK_ENVIRONMENT' => 'test',
-    //ws_941844@Company.MyWorld
-];
+$adyenCredentials = json_decode(getenv('SPRYKER_ADYEN_CREDENTIALS') ?: 'null', true);
 
 $config[AdyenConstants::SPLIT_ACCOUNT] = getenv('ADYEN_MARKETPLACE_SPLIT_PAYMENT_ACCOUNT');
 $config[AdyenConstants::SPLIT_ACCOUNT_COMMISSION_INTEREST] = 0.015;
