81d4ca404ab8a5430c4c89badf6326ddd4d709e3
hpsoftbusiness <60627656+hpsoftbusiness@users.noreply.github.com>
2021-07-18 22:53:24 +0200

    Orderview fix (#405)
    * order view fix
    
    * changes
    
    * changes

--- src/Pyz/Yves/BenefitDeal/Theme/default/components/molecules/benefit-deal-summary/benefit-deal-summary.ts
+++ src/Pyz/Yves/BenefitDeal/Theme/default/components/molecules/benefit-deal-summary/benefit-deal-summary.ts
@@ -2,14 +2,15 @@ import Component from 'ShopUi/models/component';
 import $ from 'jquery/dist/jquery';
 
 export default class BenefitDealSummary extends Component {
-    protected useShoppingPointSelector: string;
-    protected useShoppingPointIdRegex: RegExp;
-    protected benefitVouchersAmountId: string;
-    protected benefitVouchersAmountSelector: string;
-    protected recalculateRoute: string;
-    protected finalAmountSelector: string;
-    protected totalUsedShoppingPointsSelector: string;
-    protected readyCallback(): void {
+        protected useShoppingPointSelector: string;
+        protected useShoppingPointIdRegex: RegExp;
+        protected benefitVouchersAmountId: string;
+        protected benefitVouchersAmountSelector: string;
+        protected recalculateRoute: string;
+        protected finalAmountSelector: string;
+        protected invoke_make_blank: boolean;
+        protected totalUsedShoppingPointsSelector: string;
+        protected readyCallback(): void {
     }
 
     protected init(): void {
@@ -20,10 +21,23 @@ export default class BenefitDealSummary extends Component {
         this.recalculateRoute = '/calculation/recalculate';
         this.finalAmountSelector = '#benefit-deal-summary__final-amount';
         this.totalUsedShoppingPointsSelector = '.benefit-deal-summary__total-used-shopping-points';
-
         $(this.useShoppingPointSelector).prop('checked', true);
         this.recalculatePriceToPay();
         this.onInputChange();
+        this.invoke_make_blank = true;
+    }
+
+    protected makeBlank(): void {
+        if (this.invoke_make_blank)
+        {
+            var amount = Math.floor($('#benefitVoucherAmount').val()/100);
+
+            if (Number.isNaN(amount)) {
+                amount = 0;
+            }
+            $('#benefit_deal_collection_form_totalUsedBenefitVouchersAmount').val(amount);
+            this.invoke_make_blank = false;
+        }
     }
 
     protected onInputChange(): void {
@@ -39,6 +53,7 @@ export default class BenefitDealSummary extends Component {
             $(self.finalAmountSelector).text(data.totals_formatted.price_to_pay);
             self.updateTotalUsedShoppingPoints(data);
             document.getElementById(self.benefitVouchersAmountId).value = data.total_used_benefit_vouchers_amount / 100;
+            self.makeBlank();
         });
     }
 
@@ -64,4 +79,5 @@ export default class BenefitDealSummary extends Component {
         $(this.totalUsedShoppingPointsSelector).text(data.total_used_shopping_points_amount);
     }
 }
+
 
