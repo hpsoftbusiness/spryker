aef786ebea0226647e1935d913bc525a4fbf1358
Jackson88 <e.romanov88@gmail.com>
2021-07-23 14:35:27 +0300

    Master to Dev sync (#416)
    * MYW-1380: Fixing attributes
    
    * MYW-1380: Fixing coding styles
    
    * MYW-1401: Enable internal payment for production
    
    * Revert "Orderview fix (#405)"
    
    This reverts commit 81d4ca404ab8a5430c4c89badf6326ddd4d709e3.
    
    * Hotfix. checkout sms confirmation duplication (#410)
    
    * Hotfix. Avoid duplication of sms code generating: pre-check for conditions is working for all types of requests for checkout step, and submiting of sms code in this step (POST .../summary) generates new sms code again all the time and making current one invalid
    
    * Hotfix. Avoid duplication of sms code generating. CS fix
    
    * [hotfix] sync dev and prod oms config
    
    * Hotfix. Fix myworld payment api integrations (#415)
    
    * Hotfix: no need to generate sms code second time! Code will be generated and send with initialize session request to MW Payment API, generating a code will invalidate first one, and customer won't be able to use it
    
    * Hotfix: no need to generate sms code second time! Code will be generated and send with initialize session request to MW Payment API, generating a code will invalidate first one, and customer won't be able to use it (fix code style)
    
    * Hotfix: no need to generate sms code second time! Code will be generated and send with initialize session request to MW Payment API, generating a code will invalidate first one, and customer won't be able to use it (fix code style)
    
    Co-authored-by: Bunyamin Inan <buenyamin.inan@spryker.com>
    Co-authored-by: Bezpiatov <aleksandr.bezpiatov@spryker.com>

--- src/Pyz/Yves/CheckoutPage/Process/Steps/SummaryStep/PreConditionChecker.php
+++ src/Pyz/Yves/CheckoutPage/Process/Steps/SummaryStep/PreConditionChecker.php
@@ -7,49 +7,13 @@
 
 namespace Pyz\Yves\CheckoutPage\Process\Steps\SummaryStep;
 
-use Generated\Shared\Transfer\MyWorldApiRequestTransfer;
-use Generated\Shared\Transfer\PaymentCodeGenerateRequestTransfer;
 use Generated\Shared\Transfer\QuoteTransfer;
-use Pyz\Client\MyWorldPayment\MyWorldPaymentClientInterface;
 use Pyz\Shared\MyWorldPayment\MyWorldPaymentConfig;
-use Pyz\Shared\MyWorldPayment\MyWorldPaymentConstants;
 use Pyz\Yves\CheckoutPage\Process\Steps\PreConditionCheckerInterface;
-use Spryker\Yves\Messenger\FlashMessenger\FlashMessengerInterface;
-use Symfony\Contracts\Translation\TranslatorInterface;
 
 class PreConditionChecker implements PreConditionCheckerInterface
 {
     protected const KEY_MESSAGE_SMS_CODE_SEND_ERROR = 'checkout.step.summary.sms_code_send_error';
-
-    /**
-     * @var \Pyz\Client\MyWorldPayment\MyWorldPaymentClientInterface
-     */
-    private $myWorldPaymentClient;
-
-    /**
-     * @var \Spryker\Yves\Messenger\FlashMessenger\FlashMessengerInterface
-     */
-    private $flashMessenger;
-
-    /**
-     * @var \Symfony\Contracts\Translation\TranslatorInterface
-     */
-    private $translator;
-
-    /**
-     * @param \Pyz\Client\MyWorldPayment\MyWorldPaymentClientInterface $myWorldPaymentClient
-     * @param \Spryker\Yves\Messenger\FlashMessenger\FlashMessengerInterface $flashMessenger
-     * @param \Symfony\Contracts\Translation\TranslatorInterface $translator
-     */
-    public function __construct(
-        MyWorldPaymentClientInterface $myWorldPaymentClient,
-        FlashMessengerInterface $flashMessenger,
-        TranslatorInterface $translator
-    ) {
-        $this->myWorldPaymentClient = $myWorldPaymentClient;
-        $this->flashMessenger = $flashMessenger;
-        $this->translator = $translator;
-    }
 
     /**
      * @param \Generated\Shared\Transfer\QuoteTransfer $quoteTransfer
@@ -59,35 +23,6 @@ class PreConditionChecker implements PreConditionCheckerInterface
     public function check(QuoteTransfer $quoteTransfer): bool
     {
         $quoteTransfer->setSmsCode(null);
-
-        if ($this->wasPaymentSessionCreatedSuccessfully(
-            $quoteTransfer
-        ) && $quoteTransfer->getMyWorldPaymentIsSmsAuthenticationRequired()) {
-            $myWorldApiRequestTransfer = new MyWorldApiRequestTransfer();
-            $myWorldApiRequestTransfer->setPaymentCodeGenerateRequest(
-                (new PaymentCodeGenerateRequestTransfer())
-                    ->setSessionId($quoteTransfer->getMyWorldPaymentSessionId())
-            );
-
-            $myWorldApiResponseTransfer = $this->myWorldPaymentClient->sendSmsCodeToCustomer(
-                $myWorldApiRequestTransfer
-            );
-
-            if (!$myWorldApiResponseTransfer->getIsSuccess()) {
-                $myWorldApiResponseTransfer->requireError();
-
-                if ($myWorldApiResponseTransfer->getError()->getErrorCode(
-                ) === MyWorldPaymentConstants::RESPONSE_ERROR_CODE_GENERATION_NOT_CONFIGURED) {
-                    return true;
-                } else {
-                    $this->flashMessenger->addErrorMessage(
-                        $this->translator->trans(static::KEY_MESSAGE_SMS_CODE_SEND_ERROR)
-                    );
-                }
-            }
-
-            return $myWorldApiResponseTransfer->getIsSuccess();
-        }
 
         return $this->wasPaymentSessionCreatedSuccessfully($quoteTransfer) || !$this->isMyWorldPaymentUsed(
             $quoteTransfer
