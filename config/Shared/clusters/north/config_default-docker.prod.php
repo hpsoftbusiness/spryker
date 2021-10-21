7825e20fbdde6fd6fece12301cc757b717585f9a
Bünyamin Inan <bnymn@users.noreply.github.com>
2021-10-21 10:46:44 +0200

    MPL-29: Use different Adyen Marchant for Norway (#626)
    * MPL-29: Use different Adyen Marchant for Norway
    
    * MPL-29: Use different Adyen Marchant for Norway

--- config/Shared/clusters/north/config_default-docker.prod.php
+++ config/Shared/clusters/north/config_default-docker.prod.php
@@ -1,11 +1,10 @@
 <?php
-// config file for overriding by cluster
-
 
 use Pyz\Shared\Adyen\AdyenConstants;
 use Pyz\Shared\Country\CountryConstants;
 use Pyz\Shared\Locale\LocaleConstants;
 use Pyz\Shared\MyWorldMarketplaceApi\MyWorldMarketplaceApiConstants;
+use SprykerEco\Shared\AdyenApi\AdyenApiConstants;
 
 $config[CountryConstants::CLUSTER_COUNTRIES] = ['NO', 'FI', 'DK', 'SE'];
 $config[LocaleConstants::COUNTRY_TO_LOCALE_RELATIONS] = [
@@ -15,7 +14,6 @@ $config[LocaleConstants::COUNTRY_TO_LOCALE_RELATIONS] = [
     'SE' => 'sv_SE',
 ];
 
-// TODO:: create ORIGIN KEYS for prod environment
 $adyenCredentials = [
     'ORIGIN_KEYS' => [
         'NO' => getenv('ADYEN_ORIGIN_KEYS_NO') ?: '',
@@ -23,8 +21,21 @@ $adyenCredentials = [
         'DK' => getenv('ADYEN_ORIGIN_KEYS_DK') ?: '',
         'SE' => getenv('ADYEN_ORIGIN_KEYS_SE') ?: '',
     ],
+    'API_KEYS' => [
+        'NO' => getenv('ADYEN_API_KEYS_NO') ?: '',
+        'FI' => getenv('ADYEN_API_KEYS_FI') ?: '',
+        'DK' => getenv('ADYEN_API_KEYS_DK') ?: '',
+        'SE' => getenv('ADYEN_API_KEYS_SE') ?: '',
+    ],
+    'MERCHANT_ACCOUNTS' => [
+        'NO' => 'MyWorldNordicAS',
+        'FI' => '',
+        'DK' => '',
+        'SE' => '',
+    ],
 ];
-
+$config[AdyenApiConstants::API_KEY] = $adyenCredentials['API_KEYS'][APPLICATION_STORE];
+$config[AdyenConstants::MERCHANT_ACCOUNT] = $adyenCredentials['MERCHANT_ACCOUNTS'][APPLICATION_STORE];
 $config[AdyenConstants::SDK_CHECKOUT_ORIGIN_KEY] = $adyenCredentials['ORIGIN_KEYS'][APPLICATION_STORE];
 
 $config[MyWorldMarketplaceApiConstants::DEALER_ID_DEFAULT] = '8F8D1A16-E266-4906-9528-AA310068B044';
