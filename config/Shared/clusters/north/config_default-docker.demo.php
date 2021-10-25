82a891490a74347dbf6c5103850bc094617d1cef
Bünyamin Inan <bnymn@users.noreply.github.com>
2021-10-25 09:54:09 +0200

    Fix adyen for staging (#655)
    * Fix adyen for staging
    
    * Fix adyen for staging for all north cluster

--- config/Shared/clusters/north/config_default-docker.demo.php
+++ config/Shared/clusters/north/config_default-docker.demo.php
@@ -14,22 +14,24 @@ $config[LocaleConstants::COUNTRY_TO_LOCALE_RELATIONS] = [
     'DK' => 'da_DK',
     'SE' => 'sv_SE',
 ];
-//"originKeys": {
-//     "https://dk.myworld-mca.cloud.spryker.toys": "pub.v2.8216083088630330.aHR0cHM6Ly9kay5teXdvcmxkLW1jYS5jbG91ZC5zcHJ5a2VyLnRveXM.MnoAGDbiKPyqKSe2ok6Me2Aj0EemstrMIa8odi1Do5U",
-//     "https://se.myworld-mca.cloud.spryker.toys": "pub.v2.8216083088630330.aHR0cHM6Ly9zZS5teXdvcmxkLW1jYS5jbG91ZC5zcHJ5a2VyLnRveXM.r1YaGnReXBzkj0idykXeuLLLrSXCCZH8pgulns7uooY",
-//     "https://no.myworld-mca.cloud.spryker.toys": "pub.v2.8216083088630330.aHR0cHM6Ly9uby5teXdvcmxkLW1jYS5jbG91ZC5zcHJ5a2VyLnRveXM.G_DoOgvix2eWWbHeJb55nc1S0Lj1a32K-3965s0MPRo",
-//     "https://fi.myworld-mca.cloud.spryker.toys": "pub.v2.8216083088630330.aHR0cHM6Ly9maS5teXdvcmxkLW1jYS5jbG91ZC5zcHJ5a2VyLnRveXM.Fv8r0jFbz6idaj58KfcmJnNdSrKrJtAs543Ws2HuEZ0"
-//    }
+
 $adyenCredentials = [
     'ORIGIN_KEYS' => [
-        'NO' => 'pub.v2.8216083088630330.aHR0cHM6Ly9uby5teXdvcmxkLW1jYS5jbG91ZC5zcHJ5a2VyLnRveXM.G_DoOgvix2eWWbHeJb55nc1S0Lj1a32K-3965s0MPRo',
-        'FI' => 'pub.v2.8216083088630330.aHR0cHM6Ly9maS5teXdvcmxkLW1jYS5jbG91ZC5zcHJ5a2VyLnRveXM.Fv8r0jFbz6idaj58KfcmJnNdSrKrJtAs543Ws2HuEZ0',
-        'DK' => 'pub.v2.8216083088630330.aHR0cHM6Ly9kay5teXdvcmxkLW1jYS5jbG91ZC5zcHJ5a2VyLnRveXM.MnoAGDbiKPyqKSe2ok6Me2Aj0EemstrMIa8odi1Do5U',
-        'SE' => 'pub.v2.8216083088630330.aHR0cHM6Ly9zZS5teXdvcmxkLW1jYS5jbG91ZC5zcHJ5a2VyLnRveXM.r1YaGnReXBzkj0idykXeuLLLrSXCCZH8pgulns7uooY',
+        'NO' => 'pub.v2.8015571693343839.aHR0cHM6Ly93d3cubm8ubXl3b3JsZC1tY2EuY2xvdWQuc3ByeWtlci50b3lz.LfKWPKRv1hxXhL-dUrW-yQPE1dHsiXN44WqEjHz2oVA',
+        'FI' => 'pub.v2.8015571693343839.aHR0cHM6Ly93d3cuZmkubXl3b3JsZC1tY2EuY2xvdWQuc3ByeWtlci50b3lz.QAK4XdSH6j_EKTpId0B4X_BcrN4r2FvrJJdy9GFnRSg',
+        'DK' => 'pub.v2.8015571693343839.aHR0cHM6Ly93d3cuZGsubXl3b3JsZC1tY2EuY2xvdWQuc3ByeWtlci50b3lz.MghQOkvISzdIYkvUywCUwxhkC976pZjIwObFXQbrwuU',
+        'SE' => 'pub.v2.8015571693343839.aHR0cHM6Ly93d3cuc2UubXl3b3JsZC1tY2EuY2xvdWQuc3ByeWtlci50b3lz.u9Je4N-4DafMbQ7MsjYdWubDD_lRhQKYPOSXOLL7kKs',
+    ],
+    'MERCHANT_ACCOUNTS' => [
+        'NO' => 'MyWorldNordicAS',
+        'FI' => '',
+        'DK' => '',
+        'SE' => '',
     ],
 ];
 
 $config[AdyenConstants::SDK_CHECKOUT_ORIGIN_KEY] = $adyenCredentials['ORIGIN_KEYS'][APPLICATION_STORE];
+$config[AdyenConstants::MERCHANT_ACCOUNT] = $adyenCredentials['MERCHANT_ACCOUNTS'][APPLICATION_STORE];
 
 $config[MyWorldMarketplaceApiConstants::DEALER_ID_DEFAULT] = '31d25f30-e540-4825-b602-adc7009eded8';
 
